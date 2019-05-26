8feacd5afa82de92be3e1f625bcf4f7044e06bc2
Samrat De <isamrat28@gmail.com>
2019-05-26 02:13:54 -0400

    Update TCS_ADF_Deployment_CICD.ps1

--- CICD-scripts/TCS_ADF_Deployment_CICD.ps1
+++ CICD-scripts/TCS_ADF_Deployment_CICD.ps1
@@ -1,8 +1,52 @@
-$files = Get-ChildItem args[0]
-foreach ($file in $files)
+param
+(
+    [parameter(Mandatory = $false)] [String] $rootFolder,
+    [parameter(Mandatory = $false)] [String] $armTemplateJSON,
+	[parameter(Mandatory = $false)] [string] $armParameterJSON,
+    [parameter(Mandatory = $false)] [String] $ResourceGroupName,
+    [parameter(Mandatory = $false)] [String] $DataFactoryName,
+    [parameter(Mandatory = $false)] [Bool] $predeployment=$true,
+    [parameter(Mandatory = $false)] [Bool] $deleteDeployment=$false
+)
+
+#Deployment date
+$Date = Get-Date
+$Month = $date.Month; if ($Month -le 9) {$Month = "0" + $Month}
+$Day = $date.Day; if ($Day -le 9) {$Day = "0" + $Day}
+$DeploymentName = "Deploy" + "$($Date.Year)" + "$Month" + "$Day"
+
+$templateJson = Get-Content $armTemplateJSON | ConvertFrom-Json
+$resources = $templateJson.resources
+
+#Triggers 
+Write-Host "Getting triggers"
+$triggersADF = Get-AzDataFactoryV2Trigger -DataFactoryName $DataFactoryName -ResourceGroupName $ResourceGroupName
+$triggersTemplate = $resources | Where-Object { $_.type -eq "Microsoft.DataFactory/factories/triggers" }
+$triggerNames = $triggersTemplate | ForEach-Object {$_.name.Substring(37, $_.name.Length-40)}
+$activeTriggerNames = $triggersTemplate | Where-Object { $_.properties.runtimeState -eq "Started" -and ($_.properties.pipelines.Count -gt 0 -or $_.properties.pipeline.pipelineReference -ne $null)} | ForEach-Object {$_.name.Substring(37, $_.name.Length-40)}
+$deletedtriggers = $triggersADF | Where-Object { $triggerNames -notcontains $_.Name }
+$triggerstostop = $triggerNames | where { ($triggersADF | Select-Object name).name -contains $_ }
+
+if ($predeployment -eq $true) {
+    #Stop all triggers
+    Write-Host "Stopping deployed triggers"
+    $triggerstostop | ForEach-Object { 
+        Write-host "Disabling trigger " $_
+        Stop-AzDataFactoryV2Trigger -ResourceGroupName $ResourceGroupName -DataFactoryName $DataFactoryName -Name $_ -Force 
+	}
+}
+
+Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+
+if ($notPresent)
 {
-    if ($file.length -gt args[1])
-    {
-        Write-Output $file
-    }
+    # ResourceGroup doesn't exist
+	Write-Error "The resource group does not exist in the Azure Subscription.";
+	Stop-Transcript;
+	return;
+}
+else
+{
+    # ResourceGroup exist
+	New-AzResourceGroupDeployment -Debug -Verbose -Name $DeploymentName -ResourceGroupName $ResourceGroupName -TemplateFile $armTemplateJSON -TemplateParameterFile $armParameterJSON
 }
